6e418e657e08878a0f3b6c86800d21f903fff19e
Guylain wasso <wassoguylain15@gmail.com>
2022-12-02 10:18:23 +0100

    Refactor Part 2

--- Day1/calorie_Counting.js
+++ Day1/calorie_Counting.js
@@ -1,14 +1,16 @@
 const {readFileSync} = require('fs');
+
+
+
 // Fonction permettant la lecture du fichier et le replacement des sauts de ligne en espace vide  
 function ReadFile(filename) {
     const contents = readFileSync(filename, 'utf-8');
-  
     const arr = contents.replace(/\r\n/g, " ").split(" ")  
     return arr;
   }
 
   let inputClean= ReadFile('./input.txt');
-  
+  console.log(inputClean)
 // fontion permettant de faire le groupage 
 const makeGroups = array => {
 	const groups = [];
@@ -24,12 +26,11 @@ const makeGroups = array => {
 	groups.push(group);
 	return groups;
 };
-
 let getGroups= makeGroups(inputClean); 
 
 
 
-// Fonction permettant de faire la sommation des groupe 
+// Fonction permettant de faire la sommation des groupes
 let totalPerGroups=[]
 	const handleGroup = groups => {
 		const totalCaloriesPerGroup = groups.reduce((prev, curr) => {
@@ -38,11 +39,13 @@ let totalPerGroups=[]
 		totalPerGroups.push(totalCaloriesPerGroup)
 	};
 
-// Sommation des calories par groupe 
-
 	for (let i = 0; i < getGroups.length; i++) handleGroup(getGroups[i], i);
-	const largestTotal=Math.max(...totalPerGroups);  // le plus grand total 
 
+	
+// Part 1
+	const largestTotal=Math.max(...totalPerGroups);  // le plus grand total 
+console.log(largestTotal)
 // Part 2
 	const sortLargestTotal= totalPerGroups.sort((a,b)=>b-a); 
-	const sumThreeLargestTotal=sortLargestTotal[0]+sortLargestTotal[1]+sortLargestTotal[2])
+	const sumThreeLargestTotal=sortLargestTotal[0]+sortLargestTotal[1]+sortLargestTotal[2]; 
+console.log(sumThreeLargestTotal)
